bbbc3938a5f1ac7aba847b5adb823dd0d0d3087a
Criss <wuzheyun@gmail.com>
2020-12-11 00:15:31 +0200

    test

--- database/database.js
+++ database/database.js
@@ -1,4 +1,4 @@
-import { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
+import { Client } from "https://deno.land/x/postgres@v0.4.4/mod.ts";
 // import { Client } from "../deps.js";
 // import { Pool } from "../deps.js";
 import { config } from "../config/config.js";
