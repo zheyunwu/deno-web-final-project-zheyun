e6173d1026aa55f164c04caf72cfeeee1bd6ee7b
Criss <wuzheyun@gmail.com>
2020-12-11 00:00:14 +0200

    update

--- database/database.js
+++ database/database.js
@@ -1,19 +1,35 @@
-import { Pool } from "../deps.js";
+import { Client } from "../deps.js";
+// import { Pool } from "../deps.js";
 import { config } from "../config/config.js";
 
-const connectionPool = new Pool(config.database, 3);
 
+// db: use Pool
+// const connectionPool = new Pool(config.database, 3);
+
+// const executeQuery = async(query, ...args) => {
+//   const client = await connectionPool.connect();
+//   try {
+//     return await client.query(query, ...args);
+//   } catch (e) {
+//     console.log(e);
+//   } finally {
+//     client.release();
+//   }
+
+//   return null;
+// }
+
+// db: use Client
+const client = new Client(DATABASE_URL);
 const executeQuery = async(query, ...args) => {
-  const client = await connectionPool.connect();
   try {
-    return await client.query(query, ...args);
+      await client.connect();
+      return await client.query(query, ...args);
   } catch (e) {
-    console.log(e);
+      console.log(e);
   } finally {
-    client.release();
+      await client.end();
   }
-
-  return null;
 }
 
 export { executeQuery };
