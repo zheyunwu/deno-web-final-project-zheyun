b7e79c4c186e4638f130493487e0eeddb6611ab0
Criss <wuzheyun@gmail.com>
2020-12-11 00:17:18 +0200

    test

--- database/database.js
+++ database/database.js
@@ -1,36 +1,35 @@
-import { Client } from "https://deno.land/x/postgres@v0.4.4/mod.ts";
+import { Pool } from "../deps.js";
 // import { Client } from "../deps.js";
-// import { Pool } from "../deps.js";
 import { config } from "../config/config.js";
 
 
 // db: use Pool
-// const connectionPool = new Pool(config.database, 3);
+const connectionPool = new Pool(config.database, 3);
 
-// const executeQuery = async(query, ...args) => {
-//   const client = await connectionPool.connect();
-//   try {
-//     return await client.query(query, ...args);
-//   } catch (e) {
-//     console.log(e);
-//   } finally {
-//     client.release();
-//   }
+const executeQuery = async(query, ...args) => {
+  const client = await connectionPool.connect();
+  try {
+    return await client.query(query, ...args);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    client.release();
+  }
 
-//   return null;
-// }
+  return null;
+}
 
 // db: use Client
-const client = new Client(config.database);
-const executeQuery = async(query, ...args) => {
-  try {
-      await client.connect();
-      return await client.query(query, ...args);
-  } catch (e) {
-      console.log(e);
-  } finally {
-      await client.end();
-  }
-}
+// const client = new Client(config.database);
+// const executeQuery = async(query, ...args) => {
+//   try {
+//       await client.connect();
+//       return await client.query(query, ...args);
+//   } catch (e) {
+//       console.log(e);
+//   } finally {
+//       await client.end();
+//   }
+// }
 
 export { executeQuery };
--- deps.js
+++ deps.js
@@ -1,6 +1,6 @@
 export { Application, Router, send } from "https://deno.land/x/oak@v6.3.2/mod.ts";
 export { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/master/mod.ts"
-// export { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
-// export { Pool } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
+export { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
+export { Pool } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
 export { Session } from "https://deno.land/x/session@v1.0.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.1/mod.ts";
