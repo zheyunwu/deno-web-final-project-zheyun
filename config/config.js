7ae4b9845fa7c769991da933b2924535e04045d5
Criss <wuzheyun@gmail.com>
2020-12-10 23:28:32 +0200

    test

--- config/config.js
+++ config/config.js
@@ -1,17 +1,24 @@
 let config = {};
 
-if (Deno.env.toObject().DATABASE_URL) {  // heroku
-  config.database = Deno.env.toObject().DATABASE_URL;
-} else if (Deno.env.get('TEST_ENVIRONMENT')){  // env
-  config.database = {};
-} else {
-  config.database = {  // set your self
-    hostname: "",
-    database: "",
-    user: "",
-    password: "",
-    port: 5432
-  };
-}
+// if (Deno.env.toObject().DATABASE_URL) {  // heroku
+//   config.database = Deno.env.toObject().DATABASE_URL;
+// } else if (Deno.env.get('TEST_ENVIRONMENT')){  // env
+//   config.database = {};
+// } else {
+//   config.database = {  // set your self
+//     hostname: "hattie.db.elephantsql.com",
+//     database: "bjzcyihu",
+//     user: "bjzcyihu",
+//     password: "lO1IbOxWQF9NQwkMPvXSY8tMHVgU9rXd",
+//     port: 5432
+//   };
+// }
+config.database = {  // set your self
+  hostname: "hattie.db.elephantsql.com",
+  database: "bjzcyihu",
+  user: "bjzcyihu",
+  password: "lO1IbOxWQF9NQwkMPvXSY8tMHVgU9rXd",
+  port: 5432
+};
 
 export { config };
