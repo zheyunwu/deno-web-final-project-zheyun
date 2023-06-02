db9778db6bcf99244bfca477e29ea768e82b77c0
Zheyun <57496477+zheyunwu@users.noreply.github.com>
2023-06-02 22:58:44 +0200

    Update deps.js
    Use old mod

--- deps.js
+++ deps.js
@@ -1,5 +1,6 @@
 export { Application, Router, send } from "https://deno.land/x/oak@v6.3.2/mod.ts";
-export { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/master/mod.ts"
+// export { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/master/mod.ts"
+export { viewEngine, engineFactory, adapterFactory } from "https://raw.githubusercontent.com/deligenius/view-engine/9459ad16851f19ed8c6fb4e58f6e66fe25a84f1d/mod.ts"
 export { Client } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
 export { Pool } from "https://deno.land/x/postgres@v0.4.5/mod.ts";
 export { Session } from "https://deno.land/x/session@v1.0.0/mod.ts";
